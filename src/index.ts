677191e7ee8ea1b5b353a6e4bf61f962c7d24b82
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-10-31 16:47:48 -0400

    added user login and register resolvers

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import { HelloResolver } from "./resolvers/hello";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -15,7 +16,7 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [PostResolver, HelloResolver],
+      resolvers: [PostResolver, HelloResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,7 +1,8 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import path from "path";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 // Typescript data type gets more specific when we cast to a const
 export default {
@@ -9,7 +10,7 @@ export default {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lireddit",
   type: "postgresql",
   debug: !__prod__,

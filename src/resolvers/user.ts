31e0d04a129d72ec6e4ae73e125ee3eb4ba848e0
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-11-01 17:37:33 -0500

    added logout mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
+import { COOKIE_NAME } from "../constants";
 
 // Used for argument types
 @InputType()
@@ -140,5 +141,21 @@ export class UserResolver {
     return {
       user,
     };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        } else {
+          resolve(true);
+        }
+      })
+    );
   }
 }
